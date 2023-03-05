a49a1d7a228b0089d597f069cf32229165d73c98
WongYC19 <ycfkjc@Hotmail.com>
2023-03-05 22:59:39 +0800

    Add fallback env variable

--- blogging/settings.py
+++ blogging/settings.py
@@ -17,9 +17,24 @@ import environ
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-# root = environ.Path(__file__) - 3
 env = environ.Env()
 environ.Env.read_env(BASE_DIR / ".env")
+
+# explicitly specify the environment variables to use as fallbacks
+env.ENVIRON_VARIABLES = [
+    "DJANGO_ALLOWED_HOSTS",
+    "CSRF_TRUSTED_ORIGINS",
+    "SECURE_HSTS_SECONDS",
+    "SECURE_SSL_REDIRECT",
+    "SESSION_COOKIE_SECURE",
+    "SECURE_HSTS_INCLUDE_SUBDOMAINS",
+    "SECURE_HSTS_PRELOAD",
+    "DB_NAME_WEBWISE",
+    "DB_USER_WEBWISE",
+    "DB_PASSWORD_WEBWISE",
+    "DB_HOST_WEBWISE",
+    "DB_PORT_WEBWISE",
+]
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
